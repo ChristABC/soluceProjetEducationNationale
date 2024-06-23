8c032a8bc02faf3637fc535712f8cf6017485e34
Christ ASSANA <christassana@gmail.com>
2024-06-23 23:39:11 +0200

    Ajoutez des fichiers projet.

---
+++ projetEducationNationale/Cours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetEducationNationale
+{
+    public class Cours
+    {
+        //Accesseurs aux infos sur le cours donc id cours et nom
+        public int Id { get; set; }
+        public string Nom { get; set; }
+
+        public Cours(int id, string nom)
+        {
+            Id = id; 
+            Nom = nom;
+        }
+    }
+
+}
+
---
+++ projetEducationNationale/Eleve.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetEducationNationale
+{
+    public class Eleve
+    {
+        private int ID { get; set; }//Identifiant privée eleve
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+
+        //Trouver un moyen pour notes; poser questions sur les dictionnaires
+        //Ce que fait Pauline
+
+        // Creation constructeur ELEVE
+
+        public Eleve(int id, string nom, string prenom, DateTime dateDeNaissance)
+        {
+            ID = id;
+            Nom = nom;
+            Prenom = prenom;
+            DateDeNaissance = dateDeNaissance;
+        }
+
+    }
+}
